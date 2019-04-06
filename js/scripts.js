f6bcbe25fd0f23974bb307da16794d35961b2f7e
mitchnm <mitchndungu21@gmail.com>
2019-04-06 17:51:08 +0300

    added functions4

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $("#rollOne").click(() => {
-    var random1 = Math.floor((Math.random() * 6) + 1);
+    var random1 = parseFloat(Math.floor((Math.random() * 6) + 1));
     $("#outputOne").text("DICE ROLL: " + random1); 
 
 
-    
+
     var round1Total=0
     $("#roundOne").text("ROUND TOTAL: " + random1 + round1Total );
   });
