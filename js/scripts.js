d52724de38b7ee091b66c87d03237f52e5285d67
mitchnm <mitchndungu21@gmail.com>
2019-04-08 07:38:39 +0300

    updated js3

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$("input:text#playerOne").ready(function () {
+$("input:text").ready(function () {
   $("#START").click(() => {
     var nameOne = $("input:text#playerOne").val();
     $("#player1").text("PLAYER ONE: " + nameOne);
@@ -14,10 +14,12 @@ var playerOne1 = []
 var playerTwo2 = []
 var round1Total = 0
 var round2Total = 0
+var totalScore1 = 0
+var totalScore2 = 0
 
 $(document).ready(function () {
   $("#rollOne").click(() => {
-    var random1 = parseFloat(Math.floor((Math.random() * 6) + 1));
+    var random1 = Math.floor((Math.random() * 6) + 1);
     $("#outputOne").text("DICE ROLL: " + random1);
 
     if (random1 === 1) {
@@ -44,5 +46,23 @@ $(document).ready(function () {
       round2Total = random2 + round2Total;
       $("#roundTwo").text("ROUND TOTAL: " + round2Total);
     }
+  });
+});
+
+
+$(document).ready(function () {
+  $("#holdOne").click(() => {
+    var hold1 = round1Total + totalScore1;
+    round1Total = 0;
+    $("#total1Score").text("Total score: " + hold1);
+  });
+});
+
+
+$(document).ready(function () {
+  $("#holdTwo").click(() => {
+    var hold2 = round2Total + totalScore2;
+    round2Total = 0;
+    $("#total2Score").text("Total score: " + hold2);
   });
 });
